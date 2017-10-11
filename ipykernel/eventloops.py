26635bf06ab733a526c5fd4fbc52edc67f7abcee
David Brooks <dave@bcs.co.nz>
2017-10-12 10:06:06 +1300

    Use `in_event_loop` property to flag the Qt application is in its event loop, to match what we do in IPython for PythonQt.

--- ipykernel/eventloops.py
+++ ipykernel/eventloops.py
@@ -79,9 +79,9 @@ def _loop_qt(app):
     since IPython 5.2 only checks `get_ipython().active_eventloop` is defined,
     rather than if the eventloop is actually running.
     """
-    app._in_event_loop = True
+    app.setProperty('in_event_loop', 1)
     app.exec_()
-    app._in_event_loop = False
+    app.setProperty('in_event_loop', 0)
 
 
 @register_integration('qt', 'qt4')
