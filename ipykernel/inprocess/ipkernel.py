43bd9519ded605defd25a68fee75c93b8efc157c
David Brooks <dave@bcs.co.nz>
2018-12-20 13:11:04 +1300

    OpenCOR: revert commit 7c452d0 and instead ensure we don't `stop_on_error` when executing a request.

--- ipykernel/inprocess/ipkernel.py
+++ ipykernel/inprocess/ipkernel.py
@@ -71,9 +71,14 @@ class InProcessKernel(IPythonKernel):
     def __init__(self, **traits):
         super(InProcessKernel, self).__init__(**traits)
 
-        sys.stdout, sys.stderr = self.stdout, self.stderr
         self._underlying_iopub_socket.observe(self._io_dispatch, names=['message_sent'])
         self.shell.kernel = self
+
+    def execute_request(self, stream, ident, parent):
+        """ Override for temporary IO redirection. """
+        parent[u'content']['stop_on_error'] = False
+        with self._redirected_io():
+            super(InProcessKernel, self).execute_request(stream, ident, parent)
 
     def start(self):
         """ Override registration of dispatchers for streams. """
@@ -104,6 +109,19 @@ class InProcessKernel(IPythonKernel):
         while self.raw_input_str is None:
             frontend.stdin_channel.process_events()
         return self.raw_input_str
+
+    #-------------------------------------------------------------------------
+    # Protected interface
+    #-------------------------------------------------------------------------
+
+    @contextmanager
+    def _redirected_io(self):
+        """ Temporarily redirect IO to the kernel.
+        """
+        sys_stdout, sys_stderr = sys.stdout, sys.stderr
+        sys.stdout, sys.stderr = self.stdout, self.stderr
+        yield
+        sys.stdout, sys.stderr = sys_stdout, sys_stderr
 
     #------ Trait change handlers --------------------------------------------
 
